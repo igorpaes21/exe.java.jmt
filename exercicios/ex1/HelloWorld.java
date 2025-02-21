05f765e06d2e0761d4cf1d12b8ad2174d2c0f2e6
igorpaes21 <irsp2121@hmail.com>
2025-02-20 21:55:52 -0300

    cria pasta de projetos

--- exercicios/ex1/HelloWorld.java
+++ exercicios/ex1/HelloWorld.java
@@ -1,3 +1,5 @@
+package exercicios.ex1;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
---
+++ out/production/exe.java.jmt/Ex2-Soma de Valores/SomaDeValores.java
@@ -0,0 +1,24 @@
+package SomaDeValores;
+
+import java.util.Scanner;
+
+public class SomaDeValores {
+    public static void main(String[] args) {
+
+        Scanner  scanner  = new Scanner (System.in);
+
+        System.out.println("Digite um número inteiro: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Digite um número inteiro: ");
+        int numero2 = scanner.nextInt();
+
+        int soma = numero1 + numero2;
+        System.out.println("Resultado da soma: " + soma);
+
+        scanner.close();
+    }
+
+
+
+}
