d9aa98e0ca4a880d3975abc89a1a5e9aec9b68c0
igorpaes21 <irsp2121@hmail.com>
2025-02-24 19:45:13 -0300

    Atualização das variáveis

--- exercicios/ex5/MediaAluno.java
+++ exercicios/ex5/MediaAluno.java
@@ -6,27 +6,28 @@ public class MediaAluno {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        double nota_1, nota_2, nota_3, nota_4, media;
         System.out.println("Digite a 1° nota do aluno:  ");
-        double nota_1 = scan.nextDouble();
+        nota_1 = scan.nextDouble();
 
         System.out.println("Digite a 2° nota do aluno:  ");
-        double nota_2 = scan.nextDouble();
+        nota_2 = scan.nextDouble();
 
         System.out.println("Digite a 3° nota do aluno:  ");
-        double nota_3 = scan.nextDouble();
+        nota_3 = scan.nextDouble();
 
         System.out.println("Digite a 4° nota do aluno:  ");
-        double nota_4 = scan.nextDouble();
+        nota_4 = scan.nextDouble();
 
-        double media = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
+        media = (nota_1 + nota_2 + nota_3 + nota_4) / 4;
 
         System.out.println("A média do aluno é: " + media);
 
-        if(media <5.0) {
+        if (media < 5.0) {
             System.out.println("REPROVADO");
-        }else if (media <=6) {
+        } else if (media <= 6) {
             System.out.println("RECUPERAÇÃO");
-        }else {
+        } else {
             System.out.println("APROVADO");
         }
 
